53c3db20c04411a66e4d0266a14939dcd97db41a
André <Andr@DESKTOP-JIB7R5Q>
2023-01-26 16:41:27 -0300

    Lista duplamente ligada - adicionando no comeco e no fim

--- src/lista/duplamente/ligada/ListaDuplamenteLigada.java
+++ src/lista/duplamente/ligada/ListaDuplamenteLigada.java
@@ -1,5 +1,145 @@
 package lista.duplamente.ligada;
 
+import lista.ligada.Celula;
+
 public class ListaDuplamenteLigada {
 
+	private Celula ultima = null;
+	private Celula primeira = null;
+	private int totalDeElementos = 0;
+	
+	public void adicionaNoComeco (Object elemento) {
+		if (this.totalDeElementos == 0) {
+			Celula nova = new Celula(elemento);
+			this.primeira = nova;
+			this.ultima = nova;
+		} else {
+			Celula nova = new Celula (this.primeira, (Celula) elemento);
+			this.primeira.setAnterior(nova);
+			this.primeira = nova;
+			
+		}
+				
+		this.totalDeElementos ++;
+		
+		
+	}
+	
+	public void adicionaNoFim (Object elemento) {
+		
+		if (this.totalDeElementos ==0) {
+			adicionaNoComeco(elemento);
+		} else {
+
+			//criamos uma nova celula
+			Celula nova = new Celula(elemento);
+			//Apontamos a seta do proximo da ultima celula para a nova
+			this.ultima.setProximo(nova);
+			//apontamos a seta do anterior para a ultima celula
+			nova.setAnterior(this.ultima);
+			//apontamos a seta da ultima celula para a nova
+			this.ultima = nova;
+			
+			this.totalDeElementos ++;
+	}
+	}
+	
+	public boolean posicaoOcupada(int posicao) {
+		return posicao >= 0 && posicao < totalDeElementos; 
+			
+				
+	}
+	
+	public Celula pegaCelula (int posicao) {
+		if(!posicaoOcupada(posicao)) {
+			throw new IllegalArgumentException("Posicao inexistente");
+		}
+		
+		Celula atual = primeira;
+		
+		
+		for (int i = 0; i < posicao; i++) {
+			atual = atual.getProximo();
+		}
+		return atual;
+		
+		}
+
+	public void adicionaPosicao (int posicao, Object elemento) {
+		if (this.totalDeElementos == 0) {
+			adicionaNoComeco(elemento);
+		}else if (posicao == this.totalDeElementos) {
+			this.adicionaNoFim(elemento);
+		} else {
+			
+			Celula anterior = pegaCelula(posicao -1);
+			Celula proxima = anterior.getProximo();
+			
+						
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	public Object pega (int posicao ) {
+		return this.pegaCelula(posicao).getElemento();
+	}
+
+	
+	public void removeDoComeco () {
+		if(this.totalDeElementos==0) {
+			
+			throw new IllegalArgumentException("lista vazia");
+		}
+		
+		this.primeira = this.primeira.getProximo();
+		this.totalDeElementos --;
+		
+		if (this.totalDeElementos == 0) {
+			this.ultima = null;
+		}
+		
+	}
+	
+	
+	public void remove (int posicao) {
+		
+		
+		
+		
+	}
+	
+	public int tamanho () {
+		return this.totalDeElementos;
+	}
+	
+	public boolean contem (Object o) {
+		return false;
+	
+	}
+	
+	@Override
+	public String toString() {
+		if (this.totalDeElementos == 0) {
+			return "[]";
+		}
+		
+		Celula atual = primeira;
+		
+		StringBuilder builder = new StringBuilder("[");
+		
+		for (int i = 0; i < totalDeElementos; i++) {
+			builder.append(atual.getElemento());
+			builder.append(",");
+			
+			atual = atual.getProximo();
+		}
+		builder.append("]");
+		return builder.toString();
+		
+	}
 }
--- src/lista/ligada/Celula.java
+++ src/lista/ligada/Celula.java
@@ -4,7 +4,12 @@ public class Celula {
 	
 	private	Object elemento;
 	private Celula proximo;
-
+	private Celula anterior;
+	
+	public Celula(Object elemento) {
+		this(null, elemento);
+	}
+	
 	public Celula(Object elemento, Celula proximo) {
 	    this.elemento = elemento;
 	    this.proximo = proximo;
@@ -17,6 +22,15 @@ public class Celula {
 	public void setProximo(Celula proximo) {
 	    this.proximo = proximo;
 	}
+	
+	public Celula getAnterior() {
+	    return anterior;
+	}
+
+	public void setAnterior(Celula anterior) {
+	    this.anterior = anterior;
+	}
+
 
 	public Object getElemento() {
 	    return elemento;
